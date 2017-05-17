2f00bdc959b529d726401f4d7bd5a6eb4b310b4e
Nex <annexta@hotmail.com>
2017-05-16 23:26:15 -0700

    updated project

---
+++ SimpleCalculator/SimpleCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            //bug for demo purposes
+            return a * b;
+        }
+    }
+}
